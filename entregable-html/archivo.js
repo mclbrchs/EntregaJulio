386d292aa65f91331b9b820b569e9aee501fe8bd
Micaela <mclbarchiesi@gmail.com>
2023-07-25 18:49:03 -0300

    Proyecto Final

--- entregable-html/archivo.js
+++ entregable-html/archivo.js
@@ -1,8 +1,6 @@
 function mostrarValoresFormulario() {
-       // Obtener el formulario por su id
         var form = document.getElementById("myForm");
 
-        // Obtener todos los campos del formulario
         var firstName = form.elements["Name"].value;
         var lastName = form.elements["LastName"].value;
         var email = form.elements["email"].value;
@@ -10,7 +8,6 @@ function mostrarValoresFormulario() {
         var phoneNumber = form.elements["PhoneNumber"].value;
         var message = form.elements["Message"].value;
 
-        // Construir el mensaje con los valores del formulario
         var mensaje = "First Name: " + firstName + "\n" +
                       "Last Name: " + lastName + "\n" +
                       "Email Address: " + email + "\n" +
@@ -18,7 +15,6 @@ function mostrarValoresFormulario() {
                       "Phone Number: " + phoneNumber + "\n" +
                       "Message: " + message;
 
-        // Mostrar los valores en una ventana de alerta
         console.log(mensaje);
         window.alert(mensaje);
 }
